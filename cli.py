44bebb203966fdfd42202b7f526e3d628910ba8c
李炜奕 <918688502@qq.com>
2025-05-23 09:50:27 +0800

    修改声明

--- cli.py
+++ cli.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
-"""
-sysHAX 命令行工具
 
-Copyright (c) 2025 Huawei Technologies Co., Ltd.
-sysHAX is licensed under Mulan PSL v2. See LICENSE for details.
+"""
+Copyright (c) 2025-2025 Huawei Technologies Co., Ltd.
+
+sysHAX is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+    http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+PURPOSE.
+See the Mulan PSL v2 for more details.
+Created: 2025-05-23
+Desc:sysHAX 命令行工具
 """
 
 from __future__ import annotations
--- src/workflow.py
+++ src/workflow.py
@@ -1,10 +1,16 @@
 """
-调度接口模块
+Copyright (c) 2025-2025 Huawei Technologies Co., Ltd.
 
-提供统一的调度接口，用于协调和管理解码任务的分发
-
-Copyright (c) 2025 Huawei Technologies Co., Ltd.
-sysHAX is licensed under Mulan PSL v2. See LICENSE for details.
+sysHAX is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+    http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+PURPOSE.
+See the Mulan PSL v2 for more details.
+Created: 2025-05-23
+Desc:sysHAX 工作流模块
 """
 
 from __future__ import annotations
